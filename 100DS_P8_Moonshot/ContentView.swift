99a7072d026561fca1af91537285c54a359450de
Erica Sampson <ericabcherry@gmail.com>
2024-07-19 16:23:15 -0400

    Initial Commit

---
+++ 100DS_P8_Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  100DS_P8_Moonshot
+//
+//  Created by Erica Sampson on 2024-07-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DS_P8_Moonshot/_00DS_P8_MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DS_P8_MoonshotApp.swift
+//  100DS_P8_Moonshot
+//
+//  Created by Erica Sampson on 2024-07-19.
+//
+
+import SwiftUI
+
+@main
+struct _00DS_P8_MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
